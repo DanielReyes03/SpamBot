90fc2309c9ae6b568885e7bf48b6c98d26a7d79b
freyes2019035 <Daniel_reyes2003@outlook.com>
2021-02-27 12:16:52 -0600

    system verification and clipboard

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import pyautogui as py
 import time as t
+import clipboard as c
+from sys import platform
 
-t.sleep(3)
-cord = py.locateOnScreen('/Users/jose/Desktop/2.png')
-py.click(cord)
-while True:
-    py.hotkey('command', 'v')
-    py.hotkey('enter')
+def startSpamming():
+    imagePath = input('Escribe la direccion de la imagen del input en donde haremos spamm >> ')
+    textToSpamm = input('Escribe el texto con el que quieres hacer SPAM >> ')
+    c.copy(str(textToSpamm))
+    print('Cambia hacia la ventana de donde haras spamm')
+    t.sleep(5)
+    cord = py.locateOnScreen(str(imagePath))
+    py.click(cord)
+    while True:
+        if platform == "linux" or platform == "linux2":
+            py.hotkey('ctrl', 'v')
+            py.hotkey('enter')
+        elif platform == "darwin":
+            py.hotkey('command', 'v')
+            py.hotkey('enter')
+        elif platform == "win32":
+            py.hotkey('ctrl', 'v')
+            py.hotkey('enter')
+
+
+startSpamming()
