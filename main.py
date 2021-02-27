35dd9203fdafb3b7776e0f11e83916d525e0d531
freyes2019035 <Daniel_reyes2003@outlook.com>
2021-02-27 12:25:53 -0600

    spammer finished

--- main.py
+++ main.py
@@ -8,19 +8,21 @@ def startSpamming():
     textToSpamm = input('Escribe el texto con el que quieres hacer SPAM >> ')
     c.copy(str(textToSpamm))
     print('Cambia hacia la ventana de donde haras spamm')
-    t.sleep(5)
-    cord = py.locateOnScreen(str(imagePath))
-    py.click(cord)
-    while True:
-        if platform == "linux" or platform == "linux2":
-            py.hotkey('ctrl', 'v')
-            py.hotkey('enter')
-        elif platform == "darwin":
-            py.hotkey('command', 'v')
-            py.hotkey('enter')
-        elif platform == "win32":
-            py.hotkey('ctrl', 'v')
-            py.hotkey('enter')
-
-
+    for i in range(5, 0, -1):
+        print('Spamming in {}'.format(i))
+        t.sleep(1.5)
+    else:
+        cord = py.locateOnScreen(str(imagePath))
+        py.click(cord)
+        while True:
+            if platform == "linux" or platform == "linux2":
+                py.hotkey('ctrl', 'v')
+                py.hotkey('enter')
+            elif platform == "darwin":
+                py.hotkey('command', 'v')
+                py.hotkey('enter')
+            elif platform == "win32":
+                py.hotkey('ctrl', 'v')
+                py.hotkey('enter')
+                
 startSpamming()
